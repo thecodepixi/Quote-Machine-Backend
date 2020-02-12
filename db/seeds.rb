f406aece065dcd4338b81fa65d6d243235500006
Emily <emily.a.harber@gmail.com>
2020-02-11 20:33:31 -0500

    new seeds again

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Theme.all.destroy_all 
+Quote.all.destroy_all 
+
+themes = ['inspirational', 'wisdom', 'humor', 'love', 'success', 'motivational', 'joy', 'science']
+
+themes.each do |theme| 
+  Theme.create(name: theme)
+end 
 
 until Quote.all.count == 50 
-  quote = Quote.new(content: Faker::TvShows::Buffy.quote)
-  quote.author = Author.find_or_create_by(name: Faker::TvShows::Buffy.character.downcase)
-  quote.theme = Theme.all.sample 
-  quote.save 
-end 
+    quote = Quote.new(content: Faker::TvShows::Buffy.quote)
+    quote.author = Author.find_or_create_by(name: Faker::TvShows::Buffy.character.downcase)
+    quote.theme = Theme.all.sample 
+    quote.save 
+end
